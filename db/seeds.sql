198f7aa5ed9dbdbb8143aa602cede71f865b158a
Steve Garrison <stevegarrison@Steves-MacBook-Pro.local>
2019-04-14 20:03:26 -0600

    Pushing broken mvp

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Barbeque Bacon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Guac', false);
